e501cfc472b8fdcd2bde1c9a284dc61b90cd614c
Enrique Cerna <ecerna96@gmail.com>
2019-07-03 00:04:45 +0000

    added the def where the 1s will be located and their coords will be saved

--- imageblur2.rb
+++ imageblur2.rb
@@ -5,6 +5,35 @@ class Image
     @image_blur_array = array # Once a new object gets initialized, a parameter of an array will be expected and saved under 'image_blur_array'
   end
 
+  # This method will locate the ones in the array given.
+  def find_ones
+    ones = []
+    # This line will start at the beginning (upper left corner of the 2D array) so the first value, and the index of that value (i.e. [value: 0, index: 0]).
+    # This will then continue on to line 17
+    # Line 14 will grab the row item (0) and work along the columns. After first itiretaion (same as above since its the first index), the loop will continue until the
+    # last number of the columns on that row item completes. So in this case, the three zeroes at underneath the first zero.
+    # Afterwards, it will go back up to the rows line (16) and jump on to the next index and item on that row. This will continue until the entire array has been checked through.
+    @image_blur_array.each_with_index do | row_item, row_index| 
+      row_item.each_with_index do |col_item, col_index|
+        if col_item == 1 # If the col_item (since it is the last number we checked) equals to '1', the coordinates of that number will be added onto the array 'ones'
+          ones << [row_index, col_index]
+        end
+      end
+    end
+
+    # This loop is used to debug and check to see if the locations of the '1's' are saved and correct!
+    # ones.each do |row|
+    #   puts row.join
+    # end
+
+    # Returns the 'ones' array to the blur method
+    return ones
+  end
+
+  def blur
+    ones_coord = find_ones # Local save of the location of the '1's' in the array
+  end
+
   # This outputs each digit on its own line!
   # def output_image
   #   @image_blur_array.each do |row|
@@ -28,5 +57,8 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 
-# Outputs the result to the console!
+# Outputs the results to the console -> Image Blur 1!
 image.output_image
+
+# Outputs the results to the console -> Image Blur 2!
+image.blur
